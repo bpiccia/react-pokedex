57b6a1c23ecb33337b4b6c02c6fb5874792bd742
Bruno Picciafuoco <bpiccia@gmail.com>
2022-12-14 22:47:13 -0300

    basic search engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pokemonDB from "./data/pokemon.db.json";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			pokemonArray: [],
+			searchField: "",
+		};
+	}
+
+	componentDidMount() {
+		//here would be a fetch
+		const filtered = pokemonDB.filter(
+			(v, i, a) => a.findIndex((v2) => v2.id === v.id) === i
+		);
+		this.setState(() => {
+			return { pokemonArray: filtered };
+		});
+	}
+
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLowerCase();
+		this.setState(() => {
+			return { searchField };
+		});
+	};
+
+	render() {
+		const { pokemonArray, searchField } = this.state;
+		const { onSearchChange } = this;
+
+		const filteredPokemon = pokemonArray.filter((pokemon) =>
+			pokemon.slug.includes(searchField)
+		);
+
+		return (
+			<div className="App">
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search pokémon"
+					onChange={onSearchChange}
+				></input>
+				{filteredPokemon.map((pokemon) => {
+					return (
+						<div key={pokemon.id}>
+							<h1>{pokemon.name}</h1>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
